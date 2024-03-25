5edb8b1af73f69a97325a5706f67f0ba44df6f48
SGRAVANI <shakuntla.ravani@gmail.com>
2024-03-25 08:50:50 +0530

    Text Commit

---
+++ src/Components/XCounter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./styles.module.css"
+import { useState } from 'react'
+export default function XCounter() {
+    let [c,setC]=useState(0)
+  return (
+    <div className={styles.container}>
+       
+            <h1 className={styles.counterHeading}>Counter App</h1>
+            <p className={styles.counterDisplay}>Count: {c}</p>
+            <div className={styles.btn}>
+                <button onClick={setC((prev)=>prev+1)}>Increment</button>
+                <button onClick={()=>{
+                    setC((prev)=>prev-1)
+                }}>Decrement</button>
+            </div>
+      
+    </div>
+  )
+}
